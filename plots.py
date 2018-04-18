acbfb649de90bb7bd7c0d8a94c72e6c8cf46ae23
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2018-04-18 14:47:01 +0100

    Adding top-level example file
    -Lawrence

---
+++ plots.py
@@ -0,0 +1,25 @@
+import processing_tools as pt
+import sys
+
+def plots(filepath):
+
+# Init FEL plotting object
+
+    FEL = pt.ProcessedData(filepath,num_slices=50,undulator_period=0.0275,k_fact=1.13)
+
+# plot default FEL plots
+
+    plots = pt.Bokeh_Plotting(FEL)  #to save pngs you need 'npm install -g phantomjs-prebuilt'
+    plots.prepare_defaults(file_name='test')
+    plots.plot_defaults(show_html=True) #show_html=True
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv)==2:
+        fname = sys.argv[1]
+        print 'Processing file:', fname
+        plots(fname)
+    else:
+        print 'Usage: plots.py <FileName> \n'
+        sys.exit(1)
